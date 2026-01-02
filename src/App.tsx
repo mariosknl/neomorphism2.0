62c828a1a7ae8c8938b255109290e75d1992de23
Marios Kanellopoulos <mariosknl@outlook.com>
2026-01-02 11:05:04 +0200

    Add Card component with header, content, and footer; integrate Card into App component for product display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 import Button from "./components/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/Card";
 import Nav from "./components/Nav";
 
 function App() {
@@ -13,6 +21,25 @@ function App() {
         <Button variant="warning" disabled>
           Cancel
         </Button>
+
+        <div className="m-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>This is my title</CardTitle>
+              <CardDescription>This is the price of my product</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul>
+                <li>this is one benefit asdf asdfasdf</li>
+                <li>this is one benefit asdfwedf</li>
+                <li>this is one benefit</li>
+              </ul>
+            </CardContent>
+            <CardFooter>
+              <Button variant="primary">Purchase now</Button>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,137 @@
+import { mergeProps, useRender } from "@base-ui-components/react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const cardVariants = cva("rounded-2xl border-3 border-border", {
+  variants: {
+    variant: {
+      raised: "shadow-raised",
+      inset: "shadow-inset",
+    },
+  },
+  defaultVariants: {
+    variant: "raised",
+  },
+});
+
+interface CardProps
+  extends useRender.ComponentProps<"div">,
+    VariantProps<typeof cardVariants> {}
+
+export function Card(props: CardProps) {
+  const mergedProps = mergeProps(props, {
+    className: cardVariants({ variant: props.variant }),
+  });
+
+  const element = useRender({
+    defaultTagName: "div",
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
+
+// Card Header
+const cardHeaderVariants = cva("flex flex-col space-y-1.5 p-6");
+
+interface CardHeaderProps
+  extends useRender.ComponentProps<"div">,
+    VariantProps<typeof cardHeaderVariants> {}
+
+export function CardHeader(props: CardHeaderProps) {
+  const mergedProps = mergeProps(props, {
+    className: cardHeaderVariants(),
+  });
+
+  const element = useRender({
+    defaultTagName: "div",
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
+
+// Card Title
+const cardTitleVariants = cva("font-semibold leading-none tracking-tight");
+
+interface CardTitleProps
+  extends useRender.ComponentProps<"div">,
+    VariantProps<typeof cardTitleVariants> {}
+
+export function CardTitle(props: CardTitleProps) {
+  const mergedProps = mergeProps(props, {
+    className: cardTitleVariants(),
+  });
+
+  const element = useRender({
+    defaultTagName: "div",
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
+
+// Card TitleDescription
+const cardDescriptionVariants = cva("text-sm text-muted-foreground");
+
+interface CardDescriptionProps
+  extends useRender.ComponentProps<"div">,
+    VariantProps<typeof cardDescriptionVariants> {}
+
+export function CardDescription(props: CardDescriptionProps) {
+  const mergedProps = mergeProps(props, {
+    className: cardDescriptionVariants(),
+  });
+
+  const element = useRender({
+    defaultTagName: "div",
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
+
+// Card Content
+const cardContentVariants = cva("p-6 pt-0");
+
+interface CardContentProps
+  extends useRender.ComponentProps<"div">,
+    VariantProps<typeof cardContentVariants> {}
+
+export function CardContent(props: CardContentProps) {
+  const mergedProps = mergeProps(props, {
+    className: cardContentVariants(),
+  });
+
+  const element = useRender({
+    defaultTagName: "div",
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
+
+// Card Footer
+const cardFooterVariants = cva("p-6 pt-0");
+
+interface CardFooterProps
+  extends useRender.ComponentProps<"div">,
+    VariantProps<typeof cardFooterVariants> {}
+
+export function CardFooter(props: CardFooterProps) {
+  const mergedProps = mergeProps(props, {
+    className: cardFooterVariants(),
+  });
+
+  const element = useRender({
+    defaultTagName: "div",
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+}
